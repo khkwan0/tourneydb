141597f752c16a4f4d54aad7409cd14a0ef322bd
Darren Porter <darren_po@dtgsiam.com>
2020-04-13 03:10:41 +0700

    change tournament screen styles to use stylesheet

--- client/src/screens/TournamentsScreen.js
+++ client/src/screens/TournamentsScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import {Card, Title, Paragraph} from 'react-native-paper';
 import moment from 'moment-timezone';
 import LinearGradient from 'react-native-linear-gradient';
@@ -31,32 +31,24 @@ class TournamentsScreen extends Component {
         end={{x: 1, y: 0}}
         colors={['purple', '#3b5998', '#192f6a']}
         style={{flex: 1}}>
-        <View style={{alignItems: 'center', marginTop: '20%'}}>
-          <Text
-            style={{
-              color: 'white',
-              fontSize: 40,
-              fontFamily: 'PassionOne-Regular',
-            }}>
-            shootahs
-          </Text>
+        <View style={styles.headingContainer}>
+          <Text style={styles.headingText}>shootahs</Text>
         </View>
-        <View style={{alignItems: 'center', marginTop: 8}}>
-          <Text style={{color: 'white', fontSize: 15}}>
+        <View style={styles.subheadingContainer}>
+          <Text style={styles.subheadingText}>
             {this.props.route.params.game} BALL TOURNAMENTS
           </Text>
         </View>
-        <View style={{marginTop: 20, marginLeft: 20, marginRight: 20}}>
+        <View style={styles.listContainer}>
           {this.state.tournaments.map(tourney => {
             return (
-              <View key={tourney._id} style={{marginTop: 10}}>
-                <Card style={{backgroundColor: 'black'}}>
+              <View key={tourney._id} style={styles.listItemContainer}>
+                <Card style={styles.listItem}>
                   <Card.Content>
-                    <Title
-                      style={{color: 'white', fontFamily: 'NunitoSans-Black'}}>
+                    <Title style={styles.listItemTitle}>
                       {tourney.location.name}
                     </Title>
-                    <Paragraph style={{color: 'white'}}>{}</Paragraph>
+                    <Paragraph style={styles.listItemText}>{}</Paragraph>
                   </Card.Content>
                 </Card>
               </View>
@@ -69,3 +61,43 @@ class TournamentsScreen extends Component {
 }
 
 export default TournamentsScreen;
+
+const styles = StyleSheet.create({
+  headingContainer: {
+    alignItems: 'center',
+    marginTop: '20%',
+  },
+  headingText: {
+    color: 'white',
+    fontSize: 40,
+    fontFamily: 'PassionOne-Regular',
+  },
+
+  subheadingContainer: {
+    alignItems: 'center',
+    marginTop: 8,
+  },
+  subheadingText: {
+    color: 'white',
+    fontSize: 15,
+  },
+
+  listContainer: {
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  listItemContainer: {
+    marginTop: 10,
+  },
+  listItem: {
+    backgroundColor: 'black',
+  },
+  listItemTitle: {
+    color: 'white',
+    fontFamily: 'NunitoSans-Black',
+  },
+  listItemText: {
+    color: 'white',
+  },
+});
