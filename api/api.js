04adf72066e72ecb456f0123f6e2fcbf117d3d20
khkwan0 <khkwan0@gmail.com>
2020-04-13 11:47:01 +0000

    compute correct date and time for next tournament given a seed date

--- api/api.js
+++ api/api.js
@@ -1,14 +1,31 @@
-const d = require('fastify')()
+const d = require('fastify')({logger:true})
 const monk = require('monk')
+const moment = require('moment-timezone')
 
 const db = new monk('tourneydb_mongo_1/tourneydb')
 
 console.log(db)
-d.get('/', async (req, reply) => {
+d.get('/:game', async (req, reply) => {
   try {
+    let query = {game: req.params.game}
     const tournaments = db.get('tournaments')
-    const res = await tournaments.find()
-    reply.code(200).send({err: 0, msg: res})
+    const res = await tournaments.find(query)
+    const tourneys = res.map((tourney, idx) => {
+      const now = Date.now()
+      if(tourney.start_time < now) {
+        let _start_time = moment(tourney.start_time).startOf('day')
+        let remains = tourney.start_time - parseInt(_start_time.format('x'))
+        let _now = moment(now).startOf('day')
+        while (_now > _start_time) {
+          _start_time.add(tourney.frequency, 'week') 
+        }
+        _start_time.add(remains, 'milliseconds')
+        tourney.start_time = parseInt(_start_time.format('x'))
+      } 
+      return tourney
+    })
+    console.log(tourneys)
+    reply.code(200).send({err: 0, msg: tourneys})
   } catch(e) {
     reply.code(500).send()
   }
