aa5956929aa7e5b2103a600a85ca501e01c59d71
Darren Porter <darren_po@dtgsiam.com>
2020-04-14 10:36:13 +0700

    add drawer navigator and about page route

--- client/App.js
+++ client/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import MainScreen from './src/screens/MainScreen'
+import React, {Component} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import MainScreen from './src/screens/MainScreen';
+import AboutScreen from './src/screens/AboutScreen';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
-export default function App() {
-  return(
-    <NavigationContainer>
-      <MainScreen />
-    </NavigationContainer>
-  )
+const Drawer = createDrawerNavigator();
+
+export default class App extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Drawer.Navigator initialRouteName="Home">
+          <Drawer.Screen name="Home" component={MainScreen} />
+          <Drawer.Screen name="About" component={AboutScreen} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
---
+++ client/src/screens/AboutScreen.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Text, StyleSheet} from 'react-native';
+
+class AboutScreen extends Component {
+  state = {};
+
+  componentDidMount = () => {};
+
+  render() {
+    return <Text>About yo</Text>;
+  }
+}
+
+const styles = StyleSheet.create({});
+
+export default AboutScreen;
