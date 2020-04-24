83519da36b63ef9152df6eea38631651299700f0
khkwan0 <khkwan0@gmail.com>
2020-04-24 17:47:32 +0700

    moved to react hooks style

--- client/src/screens/TournamentsScreen.js
+++ client/src/screens/TournamentsScreen.js
@@ -1,45 +1,39 @@
 import React, {Component} from 'react'
-import {View, Text, TouchableOpacity, StyleSheet, ActivityIndicator} from 'react-native'
-import Modal from 'react-native-modal'
-import {Card, Title, Paragraph} from 'react-native-paper'
+import {View, Text, TouchableOpacity, StyleSheet, ActivityIndicator, SafeAreaView, ScrollView} from 'react-native'
+import {Card} from 'react-native-paper'
 import moment from 'moment-timezone'
 import LinearGradient from 'react-native-linear-gradient'
 
-class TournamentsScreen extends Component {
+const TournamentsScreen = (props) => {
+  const [tournaments, setTournaments] = React.useState([])
+  const [idx, setIdx] = React.useState(-1)
+  const [loading, setLoading] = React.useState(true)
 
-  state = {
-    tournaments: [],
-    show_details: false,
-    idx: -1,
-    loading: true,
-  }
-
-  componentDidMount = async () => {
-    try {
-      let res = await this.getDataFromServer()
-      this.setState({loading: false, tournaments: res, show_details: false})
-    } catch(e) {
-      console.log(e)
-      this.setState({loading: false})
-    }
-    this._unsubscribe = this.props.navigation.addListener('focus', async () => {
-      console.log('focus')
-      this.setState({show_details: false, loading: true}, async () => {
+  React.useEffect(() => {
+    getData = async () => {
       try {
-        let res = await this.getDataFromServer()
-        console.log('returned')
-        this.setState({loading: false, tournaments: res})
+        const res = await getDataFromServer(props.route.params.game)
+        setIdx(-1)
+        setLoading(false)
+        setTournaments(res)
       } catch(e) {
         console.log(e)
-        this.setState({loading: false})
       }
-      })
-    })
-  }
+    },
+    getData()
+    const unsubscribe = props.navigation.addListener('focus', async () => {
+      setIdx(-1)
+      setLoading(true)
+      const res = await getDataFromServer(props.route.params.game)
+      setTournaments(res)
+      setLoading(false)
+    }) 
+    return () => {unsubscribe}
+  }, [])
 
-  getDataFromServer = async () => {
+  getDataFromServer = async (game) => {
     try {
-      let res_raw = await fetch('https://api.pubgamesdb.com/games/' + this.props.route.params.game, {
+      let res_raw = await fetch('https://api.pubgamesdb.com/games/' + game, {
         method:'GET',
         headers: {
           'Accept': 'application/json'
@@ -56,93 +50,79 @@ class TournamentsScreen extends Component {
     }
   }
 
-  showDetails = (idx) => {
-    this.setState({
-      show_details: true,
-      idx: idx
-    })
-  }
-
-  hideDetails = () => {
-    this.setState({
-      show_details: false
-    })
-  }
-
-  render() {
-//    console.log(this.props.initialPosition)
-    const MyPseudoModal = () => {
-      const tourney = this.state.tournaments[this.state.idx]
-      return(
-        <View style={styles.centeredView}>
-          <View style={styles.modal}>
-            <View style={{flexGrow: 1, justifyContent: 'center', alignItems: 'center', height: '80%'}}>
-              <View>
-                <Text style={{color:'white'}}>{tourney.location.name}</Text>
-              </View>
-              <View><Text style={{color:'white'}}>{tourney.location.addr1}</Text></View>
-              <View><Text style={{color:'white'}}>{tourney.location.addr2}</Text></View>
-              <View><Text style={{color:'white'}}>{tourney.location.city}</Text></View>
-              <View><Text style={{color:'white'}}>{tourney.location.state}</Text></View>
-              <View><Text style={{color:'white'}}>{tourney.location.zip}</Text></View>
-              <View><Text style={{color:'white'}}>{tourney.location.country}</Text></View>
-              <View><Text style={{color:'white'}}>{tourney.location.phone}</Text></View>
-              <View><Text style={{color:'white'}}>{tourney.location.email}</Text></View>
-              <View><Text style={{color:'white'}}>{tourney.location.messenger_id}</Text></View>
-              <View><Text style={{color:'white'}}>{tourney.notes}</Text></View>
+  const MyPseudoModal = () => {
+    const tourney = tournaments[idx]
+    return(
+      <View style={styles.centeredView}>
+        <View style={styles.modal}>
+          <View style={{flexGrow: 1, justifyContent: 'center', alignItems: 'center', height: '80%'}}>
+            <View>
+              <Text style={{color:'white'}}>{tourney.location.name}</Text>
             </View>
-            <View style={{flex: 1, alignItems: 'center'}}>
-                <TouchableOpacity onPress={this.hideDetails} style={styles.modalButtonWide}>
-                  <View>
-                    <Text style={{color:'white'}}>CLOSE</Text>
-                  </View>
-                </TouchableOpacity>
-            </View>
+            <View><Text style={{color:'white'}}>{tourney.location.addr1}</Text></View>
+            <View><Text style={{color:'white'}}>{tourney.location.addr2}</Text></View>
+            <View><Text style={{color:'white'}}>{tourney.location.city}</Text></View>
+            <View><Text style={{color:'white'}}>{tourney.location.state}</Text></View>
+            <View><Text style={{color:'white'}}>{tourney.location.zip}</Text></View>
+            <View><Text style={{color:'white'}}>{tourney.location.country}</Text></View>
+            <View><Text style={{color:'white'}}>{tourney.location.phone}</Text></View>
+            <View><Text style={{color:'white'}}>{tourney.location.email}</Text></View>
+            <View><Text style={{color:'white'}}>{tourney.location.messenger_id}</Text></View>
+            <View><Text style={{color:'white'}}>{tourney.notes}</Text></View>
+          </View>
+          <View style={{flex: 1, alignItems: 'center'}}>
+              <TouchableOpacity onPress={() => setIdx(-1)} style={styles.modalButtonWide}>
+                <View>
+                  <Text style={{color:'white'}}>CLOSE</Text>
+                </View>
+              </TouchableOpacity>
           </View>
         </View>
-      )
-    }
-    return(
-      <LinearGradient start={{x:0, y:1}} end={{x:1, y:0}} colors={['purple', '#3b5998', '#192f6a']} style={{flex:1}}>
-        {this.state.loading &&
-          <View style={{flex: 1,justifyContent: 'center', alignItems: 'center'}}>
-            <ActivityIndicator size="large" color="#ffffff" />
-          </View>
-        }
-        {!this.state.show_details && !this.state.loading &&
-          <View>
-            <View style={{alignItems: 'center', marginTop:'20%'}}>
-              <Text style={{color:'white', fontSize:40, fontFamily:'PassionOne-Regular'}}>PUB GAMES DB</Text>
-            </View>
-            <View style={{alignItems:'center', marginTop:8}}>
-              <Text style={{color:'white', fontSize:15}}>{this.props.route.params.game} BALL TOURNAMENTS</Text>
-            </View>
-            <View style={{marginTop: 20, marginLeft: 20, marginRight:20}}>
-            {this.state.tournaments.map((tourney, idx) => {
-              return(
-                <View key={tourney._id} style={{marginTop:10}}>
-                  <Card style={{backgroundColor: 'black'}} onPress={() => this.showDetails(idx)} title={tourney.location.name}>
-                    <View style={{width:'80%', display:'flex', paddingLeft:20, paddingTop: 20, paddingBottom:20}}>
-                      <View><Text style={{color:'white', fontSize: 25}}>{tourney.location.name}</Text></View>
-                      <View><Text style={{color:'white'}}>{tourney.type.toUpperCase()}</Text></View>
-                      <View><Text style={{color:'white'}}>{moment.tz(tourney.start_time, tourney.location.timezone).format('ddd MMM Do @ h:mm a')}</Text></View>
-                      <View><Text style={{color:'white'}}>Timezone: {tourney.location.timezone}</Text></View>
-                      <View><Text style={{color:'yellow'}}>{tourney.fee} {tourney.currency}</Text></View>
-                      <View style={{display:'flex', flexDirection:'row', justifyContent:'space-between' }}><Text style={{color:'white'}}>{tourney.max_players} players</Text><Text style={{color:'white'}}>Details</Text></View>
-                    </View>
-                  </Card>
-                </View>
-              )
-            })}
-            </View>
-          </View>
-        }
-        {this.state.show_details && !this.state.loading &&
-         <MyPseudoModal /> 
-        }
-      </LinearGradient>
+      </View>
     )
   }
+  return(
+    <SafeAreaView style={{flex: 1}}>
+    <LinearGradient start={{x:0, y:1}} end={{x:1, y:0}} colors={['purple', '#3b5998', '#192f6a']} style={{flex:1}}>
+      <View style={{alignItems: 'center', marginTop:'20%'}}>
+        <Text style={{color:'white', fontSize:40, fontFamily:'PassionOne-Regular'}}>PUB GAMES DB</Text>
+      </View>
+      <View style={{alignItems:'center', marginTop:8}}>
+        <Text style={{color:'white', fontSize:15}}>{props.route.params.game} BALL TOURNAMENTS</Text>
+      </View>
+    {loading &&
+      <View style={{flex: 1,justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator size="large" color="#ffffff" />
+      </View>
+    }
+    {idx < 0 && !loading &&
+      <ScrollView>
+        <View style={{marginTop: 20, marginLeft: 20, marginRight:20}}>
+        {tournaments.map((tourney, _idx) => {
+          return(
+            <View key={tourney._id} style={{marginTop:10}}>
+              <Card style={{backgroundColor: 'black'}} onPress={() => setIdx(_idx)} title={tourney.location.name}>
+                <View style={{width:'80%', display:'flex', paddingLeft:20, paddingTop: 20, paddingBottom:20}}>
+                  <View><Text style={{color:'white', fontSize: 25}}>{tourney.location.name}</Text></View>
+                  <View><Text style={{color:'white'}}>{tourney.type.toUpperCase()}</Text></View>
+                  <View><Text style={{color:'white'}}>{moment.tz(tourney.start_time, tourney.location.timezone).format('ddd MMM Do @ h:mm a')}</Text></View>
+                  <View><Text style={{color:'white'}}>Timezone: {tourney.location.timezone}</Text></View>
+                  <View><Text style={{color:'yellow'}}>{tourney.fee} {tourney.currency}</Text></View>
+                  <View style={{display:'flex', flexDirection:'row', justifyContent:'space-between' }}><Text style={{color:'white'}}>{tourney.max_players} players</Text><Text style={{color:'white'}}>Details</Text></View>
+                </View>
+              </Card>
+            </View>
+          )
+        })}
+        </View>
+      </ScrollView>
+    }
+    {idx > -1 && !loading &&
+     <MyPseudoModal /> 
+    }
+    </LinearGradient>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -153,7 +133,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(10,10,10,0.8)',
   },
   centeredView: {
-    height: '100%',
+    flexGrow: 1,
     width: '100%',
     justifyContent: 'center',
     alignItems: 'center',
